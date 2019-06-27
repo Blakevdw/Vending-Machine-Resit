41c94c628a8cf437f563111282aca3e743a53f5f
Blake <48078206+Blakevdw@users.noreply.github.com>
2019-06-27 15:28:59 +1000

    Add files via upload

---
+++ Classes.cs
@@ -0,0 +1,80 @@
+using System;
+
+namespace VendingMachine
+{
+	public abstract class Item
+	{
+		public string Name;
+		public float Price;
+	}
+
+	public class Food : Item
+	{
+		public int GrossWeight;
+
+		public Food(string n, float p, int gw)
+		{
+			Name = n;
+			Price = p;
+			GrossWeight = gw;
+		}
+
+		public void Eat()
+		{
+			GrossWeight = 0;
+		}
+	}
+
+	public class Perishable : Food
+	{
+		public int ExpiryMonth;
+		public int ExpiryYear;
+
+		public Perishable(string n, float p, int gw, int em, int ey) : base(n, p, gw)
+		{
+			ExpiryMonth = em;
+			ExpiryYear = ey;
+		}
+	}
+
+	public class NonPerishable : Food
+	{
+		public int NetWeight;
+
+		public NonPerishable(string n, float p, int gw, int nw) : base(n, p, gw)
+		{
+			NetWeight = nw;
+		}
+	}
+
+	//----------------------------------------------------------//
+	// Use the Food classes as an example to complete Clothing,
+	// Safety and Casual.  Hint:  Think about inheritence
+	//----------------------------------------------------------//
+	public class Clothing : Item
+	{
+		public String Size;
+		public bool Used;
+
+
+		public Clothing(string n, float p, string s, bool used)
+		{
+			Size = s;
+			Used = used;
+		}
+
+
+		public Clothing(string name, float price, string size)
+		{
+			Name = name;
+			Price = price;
+			Size = size;
+		}
+
+		public void Wear()
+		{
+			Used = true;
+		}
+
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+	class Program
+	{
+
+		static List<Item> items = new List<Item>();
+
+		static void Main(string[] args)
+		{
+			float budget = getBudget();
+			string input = "";
+
+			while (input.ToLower() != "e")
+			{
+				Console.WriteLine();
+				Console.WriteLine("--------------------------");
+				Console.WriteLine("Would you like to: ");
+				Console.WriteLine("(A)dd and item");
+				Console.WriteLine("(C)heck budget");
+				Console.WriteLine("(E)xit");
+				Console.Write("Enter selection: ");
+
+				input = Console.ReadLine();
+
+				if (input.ToLower() == "a")
+				{
+					addItem();
+				}
+				else if (input.ToLower() == "c")
+				{
+					Console.WriteLine("---------------------------");
+					Console.WriteLine();
+					//------- Add Code to calculate if items exceed the budget ----//
+					float remainingbudget = budget;
+					foreach (Item i in items)
+					{
+						remainingbudget -= i.Price;
+					}
+
+					Console.WriteLine("Starting budget: $" + budget);
+					//------- Example below gets everything from items and prints name and price ----/
+					foreach (Item i in items)
+					{
+						Console.WriteLine(i.Name + "  $" + i.Price);
+					}
+
+					Console.WriteLine("budget remaining: $" + remainingbudget);
+					if (remainingbudget < 0)
+					{
+						Console.WriteLine("Warning your item total exceeds your budget");
+					}
+
+
+
+				}
+			}
+
+
+
+
+		}
+
+		static float getBudget()
+		{
+			string input = "";
+			float price = -1f;
+
+			Console.Write("Please enter budget: $");
+			input = Console.ReadLine();
+			price = float.Parse(input);
+
+			return price;
+		}
+
+		static void addItem()
+		{
+			Console.WriteLine("---------------------------");
+			Console.WriteLine();
+			string input = "";
+			string name;
+			float price = -1f;
+			string itemType = "";
+
+			Console.Write("Enter Item name: ");
+			name = Console.ReadLine();
+
+			Console.Write("Enter Item price: ");
+			input = Console.ReadLine();
+			price = float.Parse(input);
+
+			Console.Write("Enter Item type (F)ood or (C)lothing");
+			itemType = Console.ReadLine();
+
+			// Get data for food - this may be of use as an example
+			if (itemType.ToLower() == "f")
+			{
+				int grossWeight = -1;
+
+				Console.Write("Enter Gross Weight: ");
+				input = Console.ReadLine();
+				grossWeight = int.Parse(input);
+
+				Food newFood = new Food(name, price, grossWeight);
+				items.Add(newFood);
+			}
+
+			else
+
+			if (itemType.ToLower() == "c")
+			{
+				string Size = input;
+
+				Console.Write("Enter Clothing Size:");
+				input = Console.ReadLine();
+				Size = (input);
+
+				Clothing newClothing = new Clothing(name, price, Size);
+				items.Add(newClothing);
+
+
+
+			}
+
+
+		}
+
+
+
+	}
+
+
+}
